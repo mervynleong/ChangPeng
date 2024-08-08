044f57a2ed9067d9bf58228b352b2acebe552e38
mervynleong <changpeng66@gmail.com>
2024-08-08 18:20:18 +0800

    dsa

--- cp/src/components/Profile.jsx
+++ cp/src/components/Profile.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styles from "../css/Standard.module.css";
 
 const Profile = () => {
-  return <div></div>;
+  return (
+    <div className={styles.profileContainer}>
+      <div>Name: Leong Chang Peng</div>
+      <div>Gender: Male</div>
+      <div>Education:</div>
+      <ul className={styles.educationList}>
+        <li>Degree In Materials Science And Engineering</li>
+      </ul>
+      <ul className={styles.educationListNoStyle}>
+        <li>College: Nanyang Technological University</li>
+      </ul>
+      <ul className={styles.educationList}>
+        <li>Diploma in Mechanical Engineering</li>
+      </ul>
+      <ul className={styles.educationListNoStyle}>
+        <li>Polytechnic/ HighSchool: Singapore Polytechnic</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Profile;
