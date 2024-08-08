e041387654b487f29914ed9f3b40f3e088de52e0
mervynleong <changpeng66@gmail.com>
2024-08-08 16:33:28 +0800

    asd

--- cp/src/App.jsx
+++ cp/src/App.jsx
@@ -4,19 +4,22 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import NotFound from "./components/NotFound";
 import Other from "./components/Other";
+import Profile from "./components/Profile";
+import styles from "./css/Header.module.css";
 
 function App() {
   return (
     <>
-      <>
-        <NavBar />
-        <Routes>
-          <Route path="/other" element={<Other />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </>
+      <div className="container">
+        <h1>KERONIAO</h1>
+      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/other" element={<Other />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 }
